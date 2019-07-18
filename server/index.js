e5d880bbfd072afe083d224eb6f11096772326df
HABTAMU ASFAW <asfawh@uw.edu>
2019-07-18 14:53:04 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5001
+app.listen(5001);
